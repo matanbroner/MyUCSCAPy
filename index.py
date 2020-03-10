362b702a4216e9add4de1f65f36a8408c2c1f351
Matan Broner <matan_broner@nidaansystems.com>
2020-03-10 16:54:10 -0700

    made login default

--- index.py
+++ index.py
@@ -2,35 +2,36 @@ from src.driver.selenium        import generate_driver
 from src.api.my_ucsc.index      import MyUCSCApi
 from src.messenger.index         import Messenger
 
-user = 'mbroner'
-password = 'ma97ro99ra14'
+user =  'cruz-id'
+password =  'gold-password'
 
 course = {
-    "name": "Mathematical Methods for Engineers I",
-    "course_id": "62602",
-    "disc_id": None,
-    "lab_id": None,
-    "status": "Open"
+"name": "Mathematical Methods for Engineers I",
+"course_id": "62602",
+"disc_id": "62607",
+"lab_id": "62610",
+"status": "Open"
 }
 
 config = {
-    "enrollment": {
-        "quarter": "2020 Spring Quarter"
-    }
+	"enrollment": {
+		"quarter": "2020 Spring Quarter"
+	}
 }
 
-msg = Messenger({
-    "phone": "4084100240",
-    "provider": "tmobile"
+msg =  Messenger({
+	"phone": "4084100240",
+	"provider": "tmobile"
 })
-msg.login("mbroner@ucsc.edu", "ra14ro99ma97")
 
-driver = generate_driver()
+msg.login("cruz-id@ucsc.edu", "blue-password")
+driver =  generate_driver()
 
-api = MyUCSCApi(driver, config)
+api =  MyUCSCApi(driver, config)
 api.log_in(user, password)
 api.navigate_page("Enrollment")
 enrollment_module = api.module('enrollment')
+
 try:
     spots, course_link = enrollment_module.check_open_spots(course)
     taken, total = spots
